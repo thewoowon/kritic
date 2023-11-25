7497e8572c032b008b10ebc0ce36f607f494f606
thewoowon <thewoowon@naver.com>
2023-11-25 11:27:47 +0900

    feat: animation

--- components/svg/RedGradientExtra.tsx
+++ components/svg/RedGradientExtra.tsx
@@ -1,94 +1,145 @@
+import styled from "@emotion/styled";
+
 const RedGradientExtra = () => {
   return (
-    <svg
-      width="668"
-      height="835"
-      viewBox="0 0 668 835"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <rect
-        x="6.10352e-05"
-        y="834.5"
-        width="83.45"
-        height="668"
-        transform="rotate(-90 6.10352e-05 834.5)"
-        fill="#C20000"
-      />
-      <rect
-        x="6.10352e-05"
-        y="751.05"
-        width="83.45"
-        height="668"
-        transform="rotate(-90 6.10352e-05 751.05)"
-        fill="#CB2424"
-      />
-      <rect
-        x="6.10352e-05"
-        y="667.6"
-        width="83.45"
-        height="668"
-        transform="rotate(-90 6.10352e-05 667.6)"
-        fill="#D34545"
-      />
-      <rect
-        x="0.000183105"
-        y="584.15"
-        width="83.45"
-        height="668"
-        transform="rotate(-90 0.000183105 584.15)"
-        fill="#DB6666"
-      />
-      <rect
-        x="0.000183105"
-        y="500.7"
-        width="83.45"
-        height="668"
-        transform="rotate(-90 0.000183105 500.7)"
-        fill="#E38888"
-      />
-      <rect
-        x="0.000183105"
-        y="417.25"
-        width="83.45"
-        height="668"
-        transform="rotate(-90 0.000183105 417.25)"
-        fill="#EBACAC"
-      />
-      <rect
-        x="0.000183105"
-        y="333.8"
-        width="83.45"
-        height="668"
-        transform="rotate(-90 0.000183105 333.8)"
-        fill="#F4D1D1"
-      />
-      <rect
-        x="0.000183105"
-        y="250.35"
-        width="83.45"
-        height="668"
-        transform="rotate(-90 0.000183105 250.35)"
-        fill="#FAE8E8"
-      />
-      <rect
-        x="0.000305176"
-        y="166.9"
-        width="83.45"
-        height="668"
-        transform="rotate(-90 0.000305176 166.9)"
-        fill="#FDF7F7"
-      />
-      <rect
-        x="0.000305176"
-        y="83.45"
-        width="83.45"
-        height="668"
-        transform="rotate(-90 0.000305176 83.45)"
-        fill="#FEFBFB"
-      />
-    </svg>
+    <AnimationWave>
+      <svg
+        width="668"
+        height="835"
+        viewBox="0 0 668 835"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <rect
+          x="6.10352e-05"
+          y="834.5"
+          width="83.45"
+          height="668"
+          transform="rotate(-90 6.10352e-05 834.5)"
+          fill="#C20000"
+        />
+        <rect
+          x="6.10352e-05"
+          y="751.05"
+          width="83.45"
+          height="668"
+          transform="rotate(-90 6.10352e-05 751.05)"
+          fill="#CB2424"
+        />
+        <rect
+          x="6.10352e-05"
+          y="667.6"
+          width="83.45"
+          height="668"
+          transform="rotate(-90 6.10352e-05 667.6)"
+          fill="#D34545"
+        />
+        <rect
+          x="0.000183105"
+          y="584.15"
+          width="83.45"
+          height="668"
+          transform="rotate(-90 0.000183105 584.15)"
+          fill="#DB6666"
+        />
+        <rect
+          x="0.000183105"
+          y="500.7"
+          width="83.45"
+          height="668"
+          transform="rotate(-90 0.000183105 500.7)"
+          fill="#E38888"
+        />
+        <rect
+          x="0.000183105"
+          y="417.25"
+          width="83.45"
+          height="668"
+          transform="rotate(-90 0.000183105 417.25)"
+          fill="#EBACAC"
+        />
+        <rect
+          x="0.000183105"
+          y="333.8"
+          width="83.45"
+          height="668"
+          transform="rotate(-90 0.000183105 333.8)"
+          fill="#F4D1D1"
+        />
+        <rect
+          x="0.000183105"
+          y="250.35"
+          width="83.45"
+          height="668"
+          transform="rotate(-90 0.000183105 250.35)"
+          fill="#FAE8E8"
+        />
+        <rect
+          x="0.000305176"
+          y="166.9"
+          width="83.45"
+          height="668"
+          transform="rotate(-90 0.000305176 166.9)"
+          fill="#FDF7F7"
+        />
+        <rect
+          x="0.000305176"
+          y="83.45"
+          width="83.45"
+          height="668"
+          transform="rotate(-90 0.000305176 83.45)"
+          fill="#FEFBFB"
+        />
+      </svg>
+    </AnimationWave>
   );
 };
 
 export default RedGradientExtra;
+
+const AnimationWave = styled.div`
+  svg {
+    rect:nth-of-type(1) {
+      animation: wave 4s ease-in-out forwards;
+    }
+    rect:nth-of-type(2) {
+      animation: wave 4s ease-in-out 0.2s forwards;
+    }
+    rect:nth-of-type(3) {
+      animation: wave 4s ease-in-out 0.4s forwards;
+    }
+    rect:nth-of-type(4) {
+      animation: wave 4s ease-in-out 0.6s forwards;
+    }
+    rect:nth-of-type(5) {
+      animation: wave 4s ease-in-out 0.8s forwards;
+    }
+    rect:nth-of-type(6) {
+      animation: wave 4s ease-in-out 1s forwards;
+    }
+    rect:nth-of-type(7) {
+      animation: wave 4s ease-in-out 1.2s forwards;
+    }
+    rect:nth-of-type(8) {
+      animation: wave 4s ease-in-out 1.4s forwards;
+    }
+    rect:nth-of-type(9) {
+      animation: wave 4s ease-in-out 1.6s forwards;
+    }
+    rect:nth-of-type(10) {
+      animation: wave 4s ease-in-out 1.8s forwards;
+    }
+
+    @keyframes wave {
+      0% {
+        opacity: 1;
+      }
+      50% {
+        opacity: 0;
+      }
+      100% {
+        opacity: 1;
+      }
+    }
+  }
+`;
--- components/svg/RedGradientLarge.tsx
+++ components/svg/RedGradientLarge.tsx
@@ -1,24 +1,75 @@
+import styled from "@emotion/styled";
+
 const RedGradientLarge = () => {
   return (
-    <svg
-      width="719"
-      height="375"
-      viewBox="0 0 719 375"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <rect width="71.9" height="375" fill="#C20000" />
-      <rect x="71.9001" width="71.9" height="375" fill="#CB2424" />
-      <rect x="143.8" width="71.9" height="375" fill="#D34545" />
-      <rect x="215.7" width="71.9" height="375" fill="#DB6666" />
-      <rect x="287.6" width="71.9" height="375" fill="#E38888" />
-      <rect x="359.5" width="71.9" height="375" fill="#EBACAC" />
-      <rect x="431.4" width="71.9" height="375" fill="#F4D1D1" />
-      <rect x="503.3" width="71.9" height="375" fill="#FAE8E8" />
-      <rect x="575.2" width="71.9" height="375" fill="#FDF7F7" />
-      <rect x="647.1" width="71.9" height="375" fill="#FEFBFB" />
-    </svg>
+    <AnimationWave>
+      <svg
+        width="719"
+        height="375"
+        viewBox="0 0 719 375"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <rect width="71.9" height="375" fill="#C20000" />
+        <rect x="71.9001" width="71.9" height="375" fill="#CB2424" />
+        <rect x="143.8" width="71.9" height="375" fill="#D34545" />
+        <rect x="215.7" width="71.9" height="375" fill="#DB6666" />
+        <rect x="287.6" width="71.9" height="375" fill="#E38888" />
+        <rect x="359.5" width="71.9" height="375" fill="#EBACAC" />
+        <rect x="431.4" width="71.9" height="375" fill="#F4D1D1" />
+        <rect x="503.3" width="71.9" height="375" fill="#FAE8E8" />
+        <rect x="575.2" width="71.9" height="375" fill="#FDF7F7" />
+        <rect x="647.1" width="71.9" height="375" fill="#FEFBFB" />
+      </svg>
+    </AnimationWave>
   );
 };
 
 export default RedGradientLarge;
+
+const AnimationWave = styled.div`
+  svg {
+    rect:nth-of-type(1) {
+      animation: wave 4s ease-in-out forwards;
+    }
+    rect:nth-of-type(2) {
+      animation: wave 4s ease-in-out 0.2s forwards;
+    }
+    rect:nth-of-type(3) {
+      animation: wave 4s ease-in-out 0.4s forwards;
+    }
+    rect:nth-of-type(4) {
+      animation: wave 4s ease-in-out 0.6s forwards;
+    }
+    rect:nth-of-type(5) {
+      animation: wave 4s ease-in-out 0.8s forwards;
+    }
+    rect:nth-of-type(6) {
+      animation: wave 4s ease-in-out 1s forwards;
+    }
+    rect:nth-of-type(7) {
+      animation: wave 4s ease-in-out 1.2s forwards;
+    }
+    rect:nth-of-type(8) {
+      animation: wave 4s ease-in-out 1.4s forwards;
+    }
+    rect:nth-of-type(9) {
+      animation: wave 4s ease-in-out 1.6s forwards;
+    }
+    rect:nth-of-type(10) {
+      animation: wave 4s ease-in-out 1.8s forwards;
+    }
+
+    @keyframes wave {
+      0% {
+        opacity: 1;
+      }
+      50% {
+        opacity: 0;
+      }
+      100% {
+        opacity: 1;
+      }
+    }
+  }
+`;
